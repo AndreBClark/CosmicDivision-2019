dc031cc91fcfdf1f40923bac92e1d48662dd63ac
Andre Clark <abclark@cosmicdivision.design>
2019-07-05 17:34:15 -0700

    end of tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,21 +2,22 @@ module.exports = {
   siteMetadata: {
     title: 'Level Up Blog',
     description: 'A new blog!',
+    siteUrl: 'https://sharp-jang-e7f473.netlify.com',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-react-helmet',
-    // 'gatsby-plugin-sitemap',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Level Up Blog',
+        short_name: 'LUTBlog',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#524763',
+        theme_color: '#524763',
         display: 'minimal-ui',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
